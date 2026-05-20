cee9e993ac3a582f991a8a74e5cdc6d6751a866e
usaid <usaid@OSAEIDCOMPUTER>
2026-05-20 20:20:00 +0100

    Add project files.

---
+++ double dimensional array/double dimensional array.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+using namespace std;
+//.
+//
+//Write a program to input the number of students in the list.For each 
+//student enter the marks of four subjects :
+//
+//· English
+//Maths
+//· Science
+//.Computers
+//
+//Compute the average marks obtained by each student also, print the
+//student with the highest marks using the concept of double
+//dimensional array.
+
+int main()
+{
+    float maths_marks, english_marks, science_marks, computing_marks ;
+    cout << "Enter your Maths marks - ";
+    cin >> maths_marks;
+
+    cout << "Enter your English marks - ";
+    cin >> english_marks;
+    
+    cout << "Enter your Science marks - ";
+    cin >> science_marks;
+    
+    cout << "Enter your Computing marks - ";
+    cin >> computing_marks;
+
+    float average, total;
+    total = maths_marks + english_marks + science_marks + computing_marks;
+    average = total / 4;
+
+    cout << "The average marks you attained this time was - " << average << endl;
+
+
+
+
+}
